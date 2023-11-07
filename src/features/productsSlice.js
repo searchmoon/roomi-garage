531f723c3e22aafb04897871e3bcbc04a8da83da
Rumi <moonding901212@gmail.com>
2023-11-07 18:47:09 +0900

    redux-toolkit 셋팅

---
+++ src/features/productsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  searchItem: null,
+};
+
+export const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+    getSearchItem: (state, action) => {
+      state.searchItem = action.payload;
+    },
+  },
+});
+
+export const { getSearchItem } = productsSlice.actions;
+
+export default productsSlice.reducer;
